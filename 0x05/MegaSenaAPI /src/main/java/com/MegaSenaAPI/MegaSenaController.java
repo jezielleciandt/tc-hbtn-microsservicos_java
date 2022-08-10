6bc38be624535910661e534ac044414bb1a173f2
root <root@lnb036055cps.ciandt.global>
2022-08-10 17:39:17 -0300

    0. Construindo uma API REST com Spring Boot Security - MEGA SENA API

---
+++ 0x05/MegaSenaAPI /src/main/java/com/MegaSenaAPI/MegaSenaController.java
@@ -0,0 +1,27 @@
+package com.MegaSenaAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/megasena")
+public class MegaSenaController {
+
+    @GetMapping("/simpleMessageWelcome")
+    public String mensagemBoasVindas() {
+        return "Bem vindo a API REST para geração de números para a loteria Mega Sena.";
+    }
+
+
+    @GetMapping("/getNumbers")
+    public List<Integer> numerosMegaSena() {
+        Integer [] array = new Integer[6];
+        Arrays.sort(array, Collections.reverseOrder());
+        return List.of(array);
+    }
+}
